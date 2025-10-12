099fe256e5e89ad5dcc488243654c733497514d2
unknown <shorouktaha19121998@gmail.com>
2025-10-12 22:56:41 +0300

    Added my updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,22 @@
  * @returns The main content of the home page wrapped in a <main> element.
  */
 
+import About from "./_components/About/About";
+import BenefitsSection from "./_components/Benefits/BenefitsSection";
+import CriteriaSection from "./_components/Criteria/CriteriaSection";
+import Hero from "./_components/Hero/Hero";
+import Navbar from "./_components/Navbar/Navbar";
+import ProgramDetailsSection from "./_components/ProgramDetails/ProgramDetailsSection";
+
 export default function Home() {
   return (
     <main>
-      
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <ProgramDetailsSection/>
+      <BenefitsSection/>
+      <CriteriaSection/>
     </main>
   );
 }
