63e48e454bc223f7c297fd96bf06e42cf55247a8
Sarra72 <sarahmedd72@gmail.com>
2025-10-17 10:31:32 +0300

    metaData

--- src/app/changePassword/page.tsx
+++ src/app/changePassword/page.tsx
@@ -1,6 +1,13 @@
 import ProtectedRoute from '@/components/guard/ProtectPages'
 import ChangePasswordPage from '@/components/Profile/changePassword/changePass'
 import React from 'react'
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Change password",
+  description: "you can change your password easily ",
+};
+
 
 export default function page() {
   return (
