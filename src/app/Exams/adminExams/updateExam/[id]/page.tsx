0e0d0d045004fd4c64deab364efd0dd4af8a218e
abuoseed <abuoseed>
2025-10-20 22:39:35 +0300

    add protect route in update and add Exam and selected authorizetion

--- src/app/Exams/adminExams/updateExam/[id]/page.tsx
+++ src/app/Exams/adminExams/updateExam/[id]/page.tsx
@@ -1,15 +1,24 @@
 import  updateExam  from "@/components/updateExam/updateExam"
 import ProtectedRoute from "@/components/guard/ProtectPages"
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+import NotAuth from "@/components/ui/NotAuth/NotAuth";
 
 const UpdateExam=()=>{
-
+  const { userData } = useSelector((state: RootState) => state.user);
+  const isAdmin = userData?.role === "admin";
+if(isAdmin){
   return(
     <>
     <ProtectedRoute>
     <UpdateExam/>
     </ProtectedRoute>
     </>
-  )
+  )}else{
+    <>
+    <NotAuth/>
+    </>
+  }
 
 
 }
